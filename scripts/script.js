da4e0b98bfe28d3b821c798e2764e437047496c9
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-07-29 17:44:09 +0200

    added a div for contacts

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
-const ITA = {
-    whoami: "Ciao e benvenuti nel mio spazio web! Sono Frenz e sono un aspirante FrontEnd Developer, oltre al web, le mia passioni sono la fotografia, l'editing video la musica e lo snowboard"    
+const fadeInContact=()=>{
+    const contact_box = document.getElementById("contact_box");
+    contact_box.classList.add("lat1_nodelay");
+    contact_box.innerHTML="CONTATTI DOC";
 }
