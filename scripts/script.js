4bd2ad313fc9f492aa6521d13cb9311bacbe0832
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-07-29 14:58:02 +0200

    added some initial menus

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+const ITA = {
+    whoami: "Ciao e benvenuti nel mio spazio web! Sono Frenz e sono un aspirante FrontEnd Developer, oltre al web, le mia passioni sono la fotografia, l'editing video la musica e lo snowboard"    
+}
