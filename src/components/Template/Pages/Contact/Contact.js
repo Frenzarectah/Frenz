e1e9f6b0420011debecbe79568f0a37501671ceb
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-10-11 15:32:51 +0200

    added a emailJS script to make contact form works

--- src/components/Template/Pages/Contact/Contact.js
+++ src/components/Template/Pages/Contact/Contact.js
@@ -6,12 +6,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
+
 const Contact = () =>{
-    const [infoMail,setInfoMail] =useState({name:"",number:"",mail:"",message:""});
-    const sendMail = (info)=>{
-        const {name,number,mail,message} = info;
-        alert(message);
-    }
+    const referenceForm = useRef();
+    const sendEmail =(e)=>{
+        e.preventDefault();
+        emailjs.sendForm('personal', 'template_3ony9qg',referenceForm.current,'B-cYCihSV7vEgvD09')
+          .then((result) => {
+              console.log(result.text);
+              alert("email sent successfully!");
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+    
     const instaIcon = <FontAwesomeIcon icon={faInstagram} color="black" size="3x"/>
     const gitIcon = <FontAwesomeIcon icon={faGithub} color="black" size="3x"/>
     const linkedIcon = <FontAwesomeIcon icon={faLinkedin} color="black" size="3x"/>
@@ -21,18 +31,19 @@ const Contact = () =>{
         <div className="contact">
             <section id="image_container" className="animate__animated animate__heartBeat animate__infinite	infinite animate__slower"/>
             <section id="contact_sect">
-                <form className="contact__form" name="contact_form_name">
+                <form className="contact__form" ref={referenceForm} onSubmit={sendEmail} name="contact_form_name">
                     <div className="contact_wrapper">
-                        <input name="name" className="contact__form__input" type="text" placeholder="Who r u?" required onChange={e=>{setInfoMail({name:e.target.value})}}/>
-                        <input name="phone" className="contact__form__input" type="tel" placeholder="A number to call you back..." onChange={e=>{setInfoMail({number:e.target.value})}}/>
+                        <input name="name" className="contact__form__input" type="text" placeholder="Who r u?" required/>
+                        <input name="phone" className="contact__form__input" type="tel" placeholder="A number to call you back..."/>
                     </div>
                 <div className="contact_wrapper">
-                    <input name="email" className="contact__form__input _email" type="email" placeholder="...or an email to write you back." onChange={e=>{setInfoMail({mail:e.target.value})}} required/>
+                    <input name="email" className="contact__form__input _email" type="email" placeholder="...or an email to write you back."required/>
                 </div>
                 <div className="contact_wrapper">
-                    <textarea name="message" className="contact__form__input _textarea" placeholder="Say something to Frenz" required onChange={e=>{setInfoMail({message:e.target.value})}}/>
+                    <textarea name="message" className="contact__form__input _textarea" placeholder="Say something to Frenz" required/>
                 </div>
-                <Button width="100px" height="50px" font="Oswald" fontSize="20px" border="4px black solid" bkgColor="red" caption="send!" handleClick={()=>sendMail(infoMail)}/>
+                <button type="submit">submitta</button>
+                {/*<Button width="100px" height="50px" font="Oswald" fontSize="20px" border="3px solid black" bkgColor="red" caption="send!" handleClick={()=>sendMail(infoMail)}/>*/}
             </form>
             </section>
             <section id="social_sect">
