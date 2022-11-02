356b6b265cb383fd5ac64450f6094406269e3c93
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-11-02 15:25:09 +0100

    -modifying menubar in desktop vers

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,8 +7,13 @@ import './Menu.css';
 const Menu = ()=>{
     const [view,setView] = useState(false);
     
-    const hambMenu = (visibility)=> <FontAwesomeIcon icon={faHamburger} color={visibility?"white":"black"} size="3x"/>
-
+    const hambMenu = (visibility)=>{
+        if (window.matchMedia("only screen and (max-width: 600px)").matches === true){
+            return(<FontAwesomeIcon icon={faHamburger} color={visibility?"white":"black"} size="3x"/>
+        )}else{
+            return(<FontAwesomeIcon icon={faHamburger} color="black" size="3x"/>
+        )}
+    }
     let cln = "menu__container ";
       if(view)cln+="entering";
         return(
@@ -18,7 +23,7 @@ const Menu = ()=>{
             </div>
             <nav className={cln}>
                 <ul>    
-                    <li><NavLink exact="true" className="link" to="/">home</NavLink></li>
+                    <li><NavLink exact="true" className="link" to="/" onClick={()=>setView(!view)}>home</NavLink></li>
                     <li><NavLink exact="true" className={({ isActive }) => (isActive ? "actual" : "link")} to="/whoami" onClick={()=>setView(!view)}>about</NavLink></li>
                     <li><NavLink exact="true" className={({ isActive }) => (isActive ? "actual" : "link")} to="/projects" onClick={()=>setView(!view)}>projects</NavLink></li>
                     <li><NavLink exact="true" className={({ isActive }) => (isActive ? "actual" : "link")} to="/contact" onClick={()=>setView(!view)}>contacts</NavLink></li>
