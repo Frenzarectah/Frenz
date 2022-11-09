29a94201e1b07d6baf1c41ac9fab3078d1e46bac
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2022-11-09 16:05:22 +0100

    -changed scrolling text method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.scss';
 import Layout from './components/Template/Layout/Layout';
 
+/**
+ * main app returning the Layout component which contain the
+ * entire website
+ */
+
 function App() {
   return (
     <Layout/>
--- src/components/Template/Pages/Homepage/Homepage.js
+++ src/components/Template/Pages/Homepage/Homepage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import '../../../../App.scss';
 import './Homepage.css';
-import { faStar, faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Homepage = ()=>{
-    const iconBurg = <FontAwesomeIcon icon={faHamburger} color="black"/>
+    const iconstar = <FontAwesomeIcon icon={faStar} color="black"/>
     return(
         <div className="homepage">
-            <div className="roll__text">
-                <span>FRENZ {iconBurg} SORRENTINO</span>
-            </div>
+            <span className="roll__text">
+                FRENZ {iconstar} SORRENTINO
+            </span>
             <div className="subtitle">
                 <span>"Talented, Brilliant, Incredible and Amazing <s>FRONTEND DEVELOPER</s> liar"</span>
                 <span>-New York Times</span>
