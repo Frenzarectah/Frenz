27125dff36f5716038e0e12fddaffbb1fa637ade
Frenz <francesco.sorrentino@grupposinergia.com>
2023-10-06 09:17:12 +0200

    changed the spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react"
-import spinner from '../Spinner/faceSpin.gif'
+import React from "react"
 import './spinner.css'
 const Spinner = () =>{
 
     return(
-        <div className="fullpage">
-            <img alt="spinner" className="spinner" src={ spinner } />
+        <div className="content">
+            <div className="player_one"></div>
+            <div className="player_two"></div>
+            <div className="ball"></div>
         </div>
+        
     )
 }
 export default Spinner
